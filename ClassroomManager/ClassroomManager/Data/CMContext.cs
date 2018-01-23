836b95dcf053f88ccca20988eb4be5c6c9fda673
Yoni Satat <yoni.satat@gmail.com>
2018-01-23 13:22:55 +0000

    setup database connection

--- ClassroomManager/ClassroomManager/Data/CMContext.cs
+++ ClassroomManager/ClassroomManager/Data/CMContext.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using ClassroomManager.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ClassroomManager.Data
 {
     public class CMContext : DbContext
     {
-        
+        public DbSet<Absence> Absences { get; set; }
+        public CMContext(DbContextOptions<CMContext> options) : base(options) {
+            
+        }
     }
 }
--- ClassroomManager/ClassroomManager/Startup.cs
+++ ClassroomManager/ClassroomManager/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassroomManager.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +25,9 @@ namespace ClassroomManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CMContext>(context => {
+                context.UseNpgsql("Host=localhost;Database=classroom_manager");
+            });
             services.AddMvc();
         }
 
