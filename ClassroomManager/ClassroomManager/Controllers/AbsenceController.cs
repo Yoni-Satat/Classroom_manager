887befdf058d62bbe11fc8e6072d09858801828a
Yoni Satat <yoni.satat@gmail.com>
2018-01-23 13:08:17 +0000

    created absence controller

---
+++ ClassroomManager/ClassroomManager/Controllers/AbsenceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassroomManager.Data;
+using ClassroomManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassroomManager.Controllers
+{
+    [Route("api/[controller]")]
+    public class AbsenceController : Controller
+    {
+        public CMContext CMContext { get; set; }
+
+        public AbsenceController(CMContext cmContext)
+        {
+            CMContext = cmContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Absence> Get() {
+            return CMContext.Absences.ToList();
+        }
+    }
+}
