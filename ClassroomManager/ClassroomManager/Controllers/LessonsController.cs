a31e6cb924e346930e9cfadff6352da95d516aef
Yoni Satat <yoni.satat@gmail.com>
2018-01-24 16:33:43 +0000

    added LessonController

---
+++ ClassroomManager/ClassroomManager/Controllers/LessonsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClassroomManager.Data;
+using ClassroomManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassroomManager.Controllers
+{
+    [Route("api/[controller]")]
+    public class LessonsController : Controller
+    {
+        public CMContext CMContext { get; set; }
+
+        public LessonsController(CMContext cmContext)
+        {
+            CMContext = cmContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Lesson> Get() {
+            return CMContext.Lessons.ToList();
+        }
+    }
+}
