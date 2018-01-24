b8b8750fdde18dbdd4c5c63003c741ce28c4f51a
Yoni Satat <yoni.satat@gmail.com>
2018-01-24 16:11:34 +0000

    added Lesson class

---
+++ ClassroomManager/ClassroomManager/Models/Lesson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ClassroomManager.Models
+{
+    public class Lesson
+    {
+        public int ID { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public bool isMandatory { get; set; }
+        public int index { get; set; }
+        public string location { get; set; }
+        public int CourseID { get; set; }
+        public Course course { get; set; }
+    }
+}
