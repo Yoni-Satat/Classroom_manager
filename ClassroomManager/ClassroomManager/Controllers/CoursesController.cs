49007983c0ad6dbc34a4174d53154ddd3fa00bd1
Marta <martadabrowka@gmail.com>
2018-01-24 15:05:26 +0000

    added a courses controller

---
+++ ClassroomManager/ClassroomManager/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassroomManager.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassroomManager.Controllers
+{
+    [Route("api/[controller]")]
+    public class CoursesController : Controller
+    {
+        public CMContext CMContext { get; set; }
+
+        public CoursesController(CMContext cmContext)
+        {
+            CMContext = cmContext;
+        }
+    }
+}
