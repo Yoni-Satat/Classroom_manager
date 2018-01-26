5984a97ec40665eb9506215b02c19053a0e25f0f
Yoni Satat <yoni.satat@gmail.com>
2018-01-26 13:41:55 +0000

    added get by id

--- ClassroomManager/ClassroomManager/Controllers/AttendancesController.cs
+++ ClassroomManager/ClassroomManager/Controllers/AttendancesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using ClassroomManager.Data;
 using ClassroomManager.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClassroomManager.Controllers
 {
@@ -17,8 +18,27 @@ namespace ClassroomManager.Controllers
             CMContext = cmContext;
         }
         [HttpGet]
-        public void Get() {
-            //return CMContext.Attendances.ToList();
+        public IEnumerable<Attendance> Get() {
+            return CMContext.Attendances.ToList();
+        }
+
+        [HttpPost]
+        public Attendance Post([FromBody] Attendance attendance) {
+            CMContext.Attendances.Add(attendance);
+            CMContext.SaveChanges();
+            return attendance;
+        }
+
+        [HttpGet("{id}")]
+        public Attendance Get(int id) {
+            var foundAttendance = CMContext.Attendances
+                                           .Include((attendance) => attendance.student)
+                                           .Include((attendance) => attendance.lesson)
+                                           .Include((attendance) => attendance.course)
+                                           .Include((attendance) => attendance.reason)
+                                           .Where((attendance) => id == attendance.ID)
+                                           .FirstOrDefault();
+            return foundAttendance;
         }
     }
 }
